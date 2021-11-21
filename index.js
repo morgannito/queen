8dcb0718691da05977801fda35573b4615c58fa5
morgannito <mriu.morgannito@gmail.com>
2021-11-21 21:07:45 +0100

    opnezz

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-tab = [];
-nb_queen = 4;
-i = 0;
-while (i < nb_queen){
-    j = 0;
-    ligne = []
-    while (j<nb_queen){
-        ligne.push("0");
-        j= j+1;
-    }
-    tab.push(ligne)
-    i= i+1;
-}
+var cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-function add_queen(tab) {
-    tab.forEach(i => {
-        test = 0;
-        i.forEach(mycase =>{
-            if (mycase == "0"){
-            }else{
-                test = 1;
+nb_noeud = 0
+nb_feuille = 0
+nb_solution = 0
+n = 11
+board = []
+
+function test(board) {
+    for (let l1 = 0; l1 < board.length; l1++) {
+        for (let l2 = 0; l2 < l1; l2++) {
+            c1 = board[l1]
+            c2 = board[l2]
+            if (c1 == c2) {
+                return false
             }
-        })
-        if (test == 0){
-            random = getRandom(nb_queen);
-            tab[tab.indexOf(i)][random] = "1";
+            if (Math.abs(c1 - c2) == Math.abs(l1 - l2)) {
+                return false
+            }
         }
-    })
-    return tab
+    }
+    return true
 }
 
-function queen_is_here(tab){
-    for (i in tab){
-        if (i === "1"){
-            return false;
+function place(board, line, max) {
+    for (let step = 0; step < max; step++) {
+        board[line] = step
+        nb_noeud = nb_noeud + 1
+        if (line + 1 < max) {
+            place(board, line + 1, max)
+        } else {
+            nb_feuille = nb_feuille + 1
+            if (test(board)) {
+                nb_solution = nb_solution + 1
+            }
         }
-            return true
     }
 }
-function getRandom(max) {
-    return Math.floor(Math.random() * max);
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    console.time('test')
+    place(board, 0, n)
+    console.timeEnd('test');
+    console.log("n = ",nb_solution,"nombre de noeud =",nb_noeud,"nombre de feuille =",nb_feuille)
 }
 
-function eat_queen(tab){
-
+for (let step = 0; step < n; step++) {
+    board.push("0")
 }
-tab = add_queen(tab)
-console.log(tab)
+
